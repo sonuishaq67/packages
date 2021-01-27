141888518dc99b97623b90df64d1e6555528d362
sonu ishaq <sonuishaq67@gmail.com>
2021-01-27 17:17:48 +0530

    publish

---
+++ publish.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+echo "$private_key" > ~/.ssh/aur
+
+echo "
+Host aur.archlinux.org
+  IdentityFile ~/.ssh/aur
+  User aur" >> ~/.ssh/config
+  
+if [ -d deskreen-aur ]
+then
+rm -rf deskreen-aur
+fi
+
+
+git clone ssh://aur@aur.archlinux.org/deskreen.git deskreen-aur
+cd deskreen-aur
+sed -i "/pkgver=/c\pkgver=$version" PKGBUILD
+
+echo "$version"
+_pkgname=Deskreen
+pkgname=deskreen
+pkgver=$version
+
+wget https://github.com/pavlobu/${pkgname}/releases/download/v${pkgver//_/-}/${_pkgname}-${pkgver}.AppImage
+arr=(`sha256sum *.AppImage`)
+
+echo $arr
+
+
+sed -i "20s/.*/ '$arr'  /" PKGBUILD
+
+makepkg --printsrcinfo > .SRCINFO
+
+git add .
+git config user.name "$name"
+git config user.email "$email"
+git commit -m "updated pkgbuild to $version"
+git push
+
+rm ~/.ssh/aur
